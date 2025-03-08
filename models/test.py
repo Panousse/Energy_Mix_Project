2d0f5e84c930fbb3f337db0ac288ec19da3d2a74
Amine Lakchini <amine.lakchini6@gmail.com>
2025-03-08 14:38:16 +0100

    test model

---
+++ models/test.py
@@ -0,0 +1,13 @@
+# Pour pousser le repo
+
+## model_1.py
+
+# from data_prep import data_func
+# from Lin import training_lin
+# from XGB import training_xgb
+
+# X, X, y, y = data_func()
+
+# model = Linea
+
+# make_pipeline(data_func, training_lin, )
