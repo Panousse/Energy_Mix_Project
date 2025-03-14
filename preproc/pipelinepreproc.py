e98d3443ae5318a81f63372790acd47f5c3eeb7f
Aymeric THIBAUT <aymeric.thibaut@hotmail.fr>
2025-03-14 21:22:34 +0100

    global changes to insert regions in the process

--- preproc/pipelinepreproc.py
+++ preproc/pipelinepreproc.py
@@ -6,6 +6,7 @@ from preproc.function_aggregating_MF import agregating_meteo_france_data
 from preproc.function_merge_flows import consolidation_data
 from preproc.function_resampling_MF import resampling_meteo_france_data
 from preproc.function_resampling_NASA import conversion_30mins
+from preproc.function_merge_region_moy_NASA import merge30mins_regions
 
 if __name__=="__main__":
     get_meteo_france_data_from_web(2013, 2023)
@@ -13,6 +14,7 @@ if __name__=="__main__":
     call_api_nasa_yearly()
     resampling_meteo_france_data(frequency='30 min',method='interpolate')
     conversion_30mins()
+    merge30mins_regions()
     get_energy_data_from_web()
     convert_energy_data()
     consolidation_data()
